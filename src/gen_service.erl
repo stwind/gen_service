7ec41c034d5194f30e61409ac18f5e2c51e25e78
stwind <stwindfy@gmail.com>
2013-03-26 20:42:23 +0800

    Update basic setup

--- src/gen_service.erl
+++ src/gen_service.erl
@@ -1,10 +1,19 @@
 -module(gen_service).
 
+-export([start/0]).
+-export([stop/0]).
+
 -export([call/3]).
 
 %% ===================================================================
 %% Public
 %% ===================================================================
+
+start() ->
+    application:start(?MODULE).
+
+stop() ->
+    application:stop(?MODULE).
 
 call(_, _, _) ->
     ok.
